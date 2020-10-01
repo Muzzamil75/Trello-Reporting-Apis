e413fdcf529e539869350809d21ba0abc99d32bd
Muzamil Ijaz <muzamil.ijaz11@gmail.com>
2020-10-01 15:50:47 +0500

    Updates

---
+++ precache-manifest.83999d39a6dfcf4d2f8bb79d093c43db.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e672d3c0f62a7422b205065b7a73afd1",
+    "url": "/Trello-Reporting-Apis/index.html"
+  },
+  {
+    "revision": "4e1100b77eaa6c58d449",
+    "url": "/Trello-Reporting-Apis/static/js/2.8a36e3b8.chunk.js"
+  },
+  {
+    "revision": "8ae279e0396a82bcb324f2e1f01716e9",
+    "url": "/Trello-Reporting-Apis/static/js/2.8a36e3b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "04458d99f895b39a59aa",
+    "url": "/Trello-Reporting-Apis/static/js/main.737b242b.chunk.js"
+  },
+  {
+    "revision": "bcbd7f838216d6d95822",
+    "url": "/Trello-Reporting-Apis/static/js/runtime-main.d896aed4.js"
+  }
+]);
